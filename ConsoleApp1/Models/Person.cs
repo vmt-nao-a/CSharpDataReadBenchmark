27e53d1bf38425b3a2441b73383bfc41f0e1b253
Nao.A <nao.akakura@valtes-mt.co.jp>
2020-12-20 14:40:07 +0900

    first commit

---
+++ ConsoleApp1/Models/Person.cs
@@ -0,0 +1,86 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    [Table(Name = "t_person")]
+    [MessagePack.MessagePackObject]
+    public class Person
+    {
+        /// <summary>  Setting this as a starting value for id</summary>
+        private static int _userId = 100;
+
+        [Column(Name = "id", DbType = "INT", CanBeNull = false, IsPrimaryKey = true)]
+        [MessagePack.Key(0)]
+        public int Id { get; set; }
+
+        [Column(Name = "first_name", DbType = "NVARCHAR", CanBeNull = false)]
+        [MessagePack.Key(1)]
+        public string FirstName { get; set; }
+
+        [Column(Name = "middle_name", DbType = "NVARCHAR", CanBeNull = true)]
+        [MessagePack.Key(2)]
+        public string MiddleName { get; set; }
+
+        [Column(Name = "last_name", DbType = "NVARCHAR", CanBeNull = false)]
+        [MessagePack.Key(3)]
+        public string LastName { get; set; }
+
+        [Column(Name = "title", DbType = "NVARCHAR", CanBeNull = true)]
+        [MessagePack.Key(4)]
+        public string Title { get; set; }
+
+        [Column(Name = "dob", DbType = "NVARCHAR", CanBeNull = true)]
+        [MessagePack.Key(5)]
+        public DateTime DOB { get; set; }
+
+        [Column(Name = "email", DbType = "NVARCHAR", CanBeNull = true)]
+        [MessagePack.Key(6)]
+        public string Email { get; set; }
+
+        [Column(Name = "gender", DbType = "INT", CanBeNull = true)]
+        [MessagePack.Key(7)]
+        public Gender Gender { get; set; }
+
+        [Column(Name = "ssn", DbType = "NVARCHAR", CanBeNull = true)]
+        [MessagePack.Key(8)]
+        public string SSN { get; set; }
+
+        [Column(Name = "suffix", DbType = "NVARCHAR", CanBeNull = true)]
+        [MessagePack.Key(9)]
+        public string Suffix { get; set; }
+
+        [Column(Name = "phone", DbType = "NVARCHAR", CanBeNull = true)]
+        [MessagePack.Key(10)]
+        public string Phone { get; set; }
+
+        /// <summary>Gets the fake data.</summary>
+        /// <value>The fake data.</value>
+        public static Faker<Person> FakeData { get; } =
+            new Faker<Person>()
+                .RuleFor(p => p.Id, f => _userId++)
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.MiddleName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName())
+                .RuleFor(p => p.Title, f => f.Name.Prefix(f.Person.Gender))
+                .RuleFor(p => p.Suffix, f => f.Name.Suffix())
+                .RuleFor(p => p.Email, (f, p) => f.Internet.Email(p.FirstName, p.LastName))
+                .RuleFor(p => p.DOB, f => f.Date.Past(18))
+                .RuleFor(p => p.Gender, f => f.PickRandom<Gender>())
+                .RuleFor(p => p.SSN, f => f.Random.Replace("###-##-####"))
+                .RuleFor(p => p.Phone, f => f.Phone.PhoneNumber("(###)-###-####"));
+
+    }
+    public enum Gender : int
+    {
+        None = 0,
+        Male,
+        Female,
+        Other
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using ConsoleApp1.Models;
+using MessagePack;
+using Microsoft.Data.Sqlite;
+using System.Data.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Benchmark!");
+
+            const string testFileNameJson = @".\testdata.json";
+            const string testFileNameMP =  @".\testdata.msg";
+            const string testFileNameSQLite =  @".\testdata.sqlite";
+
+            if (!File.Exists(testFileNameJson) || !File.Exists(testFileNameMP) || !File.Exists(testFileNameSQLite))
+            {
+                Console.WriteLine("begin migration");
+
+                // create 0.1 million test data
+                var testData = Person.FakeData.Generate(100000).ToList();
+
+                if (!File.Exists(testFileNameJson))
+                {
+                    Console.WriteLine("write to json");
+                    var json = Utf8Json.JsonSerializer.Serialize<IEnumerable<Person>>(testData);
+                    using (var writer = new FileStream(testFileNameJson, FileMode.CreateNew))
+                    {
+                        writer.Write(json);
+                    }
+                }
+
+                if (!File.Exists(testFileNameMP))
+                {
+                    Console.WriteLine("write to MessagePack");
+                    var msg = MessagePackSerializer.Serialize<IEnumerable<Person>>(testData);
+                    using (var writer = new FileStream(testFileNameMP, FileMode.CreateNew))
+                    {
+                        writer.Write(msg);
+                    }
+                }
+                if (!File.Exists(testFileNameSQLite))
+                {
+                    Console.WriteLine("insert to SQLite");
+                    SqliteConnectionStringBuilder builder = new() { DataSource = testFileNameSQLite };
+                    using (var connection = new SqliteConnection(builder.ToString()))
+                    {
+                        connection.Open();
+                        using (var cmd = connection.CreateCommand())
+                        {
+                            cmd.CommandText = @"
+                                CREATE TABLE IF NOT EXISTS t_person (
+                                    Id INTEGER NOT NULL PRIMARY KEY,
+                                    first_name TEXT NOT NULL,
+                                    middle_name TEXT,
+                                    last_name TEXT NOT NULL,
+                                    title TEXT,
+                                    dob TEXT,
+                                    email TEXT,
+                                    gender INTEGER,
+                                    ssn TEXT,
+                                    suffix TEXT,
+                                    phone TEXT
+                            );
+                            ";
+
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        using (DataContext context = new(connection))
+                        {
+                            var table = context.GetTable<Person>();
+                            foreach (var t in testData) {
+                                table.InsertOnSubmit(t);
+                            }
+                            context.SubmitChanges();
+                        }
+                    }
+                }
+
+                Console.WriteLine("end migration");
+            }
+
+            Console.WriteLine("begin measure for read from json file (between read file to stdout from entity)");
+            var beginReadFromJsonFileDateTime = DateTime.Now;
+
+            using (var stream = new StreamReader(testFileNameJson))
+            {
+                var persons = Utf8Json.JsonSerializer.Deserialize<IEnumerable<Person>>(stream.BaseStream);
+                foreach (var p in persons)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+
+            var endReadFromJsonFileDateTime = DateTime.Now;
+            Console.WriteLine("end measure for read from json file (between read file to stdout from entity)");
+
+            Console.WriteLine("begin measure for read from Message-Pack file (between read file to stdout from entity)");
+            var beginReadFromMessagePackFileDateTime = DateTime.Now;
+
+            using (var stream = new StreamReader(testFileNameMP))
+            {
+                var persons = MessagePackSerializer.Deserialize<IEnumerable<Person>>(stream.BaseStream);
+                foreach (var p in persons)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+            var endReadFromMessagePackFileDateTime = DateTime.Now;
+            Console.WriteLine("end measure for read from Message-Pack file (between read file to stdout from entity)");
+
+            Console.WriteLine("begin measure for read from SQLite (between read file to stdout from entity)");
+            var beginReadFromSQLiteFileDateTime = DateTime.Now;
+
+            SqliteConnectionStringBuilder sqliteConnectionStringBuilder = new() { DataSource = testFileNameSQLite };
+            using (var connection = new SqliteConnection(sqliteConnectionStringBuilder.ToString()))
+            {
+                connection.Open();
+                using (DataContext context = new(connection))
+                {
+                    var table = context.GetTable<Person>();
+                    foreach (var t in table) {
+                        Console.WriteLine(t.ToString());
+                    }
+                }
+            }
+            var endReadFromSQLiteFileDateTime = DateTime.Now;
+            Console.WriteLine("end measure for read from SQLite (between read file to stdout from entity)");
+
+            var diffReadFromJsonFileDateTime = endReadFromJsonFileDateTime - beginReadFromJsonFileDateTime;
+            var diffReadFromMessagePackFileDateTime = endReadFromMessagePackFileDateTime - beginReadFromMessagePackFileDateTime;
+            var diffReadFromSQLiteFileDateTime = endReadFromSQLiteFileDateTime - beginReadFromSQLiteFileDateTime;
+
+            Console.WriteLine(@"\n\n");
+            Console.WriteLine($@"diff Json Time        : ${diffReadFromJsonFileDateTime.ToString()}");
+            Console.WriteLine($@"diff MessagePack Time : ${diffReadFromMessagePackFileDateTime.ToString()}");
+            Console.WriteLine($@"diff SQLite Time      : ${diffReadFromSQLiteFileDateTime.ToString()}");
+        }
+    }
+}
